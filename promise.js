faff1cd24d1ba412a339c415e03ff928d2b2aab4
DavidLeeeee <david431385@gmail.com>
2024-10-11 00:12:37 +0900

    third commit

--- promise.js
+++ promise.js
@@ -4,42 +4,62 @@ class seokheePromise {
   constructor(work) {
     this.state = "pending";
     this.returnV = undefined;
-    this.done = false;
+    this.callback = null;
 
     work(this.resolve, this.reject);
   }
 
   resolve = function (value) {
-    if (this.done) return;
-    this.done = true;
-    this.state = "fulfilled";
-    this.returnV = value;
+    if (this.state === "pending") {
+      this.state = "fulfilled";
+      this.returnV = value;
+
+      if (this.callback) {
+        this.callback(this.returnV);
+      }
+    }
   }.bind(this);
 
   reject = function (error) {
-    if (this.done) return;
-    this.done = true;
-    this.state = "rejected";
-    this.returnV = error;
+    if (this.state === "pending") {
+      this.state = "rejected";
+      this.returnV = error;
+
+      if (this.callback) {
+        this.callback(this.returnV);
+      }
+    }
   }.bind(this);
 
   then(callback) {
-    callback(this.returnV);
+    this.callback = callback;
+    if (this.state === "fulfilled") {
+      this.callback(this.returnV);
+    }
+    return this;
+  }
+
+  catch(callback) {
+    this.callback = callback;
+    if (this.state === "rejected") {
+      this.callback(this.returnV);
+    }
+    return this;
   }
 }
 
 const promise1 = new seokheePromise(function (resolve, reject) {
-  //resolve 또는 reject중 한개가 나오면 그대로 해당 Promise는 끝나게
-  setTimeout(() => {
-    resolve(console.log("결과드가자"));
-  }, 1000);
-
-  reject("실패!");
+  try {
+    setTimeout(() => {
+      reject("실패");
+      resolve("성공!");
+    }, 1000);
+  } catch {}
 });
 
-console.log(promise1); // 여기에서 pending이 나오도록 설계하면 좋을 듯.
+console.log(promise1); // 여기에서 pending이 나오도록
 
-promise1.then(function (result) {
-  console.log("Promise 결과:", result); // "성공!"이 출력됨
-});
+promise1
+  .then((result) => console.log("Promise 성공:", result))
+  .catch((error) => console.log("Promise 실패:", error));
 // console.log(promise1.state, "/ return: ", promise1.returnValue); // Outputs: Success!
