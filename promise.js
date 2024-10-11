59f5eed600a856ed8109938a2dbe21b2b51cce1b
이석희 <david127@yettiesoft.com>
2024-10-11 12:30:44 +0900

    basic promise work

--- promise.js
+++ promise.js
@@ -3,8 +3,9 @@
 class seokheePromise {
   constructor(work) {
     this.state = "pending";
-    this.returnV = undefined;
-    this.callback = null;
+    this.result = undefined;
+    this.callback = [];
+    this.errcallback = []; //callback공유하면.. 덮어써버리게 됨
 
     work(this.resolve, this.reject);
   }
@@ -12,38 +13,28 @@ class seokheePromise {
   resolve = function (value) {
     if (this.state === "pending") {
       this.state = "fulfilled";
-      this.returnV = value;
-
-      if (this.callback) {
-        this.callback(this.returnV);
-      }
+      this.result = value;
+      this.callback.forEach((f) => f(this.result));
     }
   }.bind(this);
 
   reject = function (error) {
     if (this.state === "pending") {
       this.state = "rejected";
-      this.returnV = error;
-
-      if (this.callback) {
-        this.callback(this.returnV);
-      }
+      this.result = error;
+      this.errcallback.forEach((f) => f(this.result));
     }
   }.bind(this);
 
   then(callback) {
-    this.callback = callback;
-    if (this.state === "fulfilled") {
-      this.callback(this.returnV);
-    }
+    this.callback.push(callback);
+    console.log("zz", this);
     return this;
   }
 
   catch(callback) {
-    this.callback = callback;
-    if (this.state === "rejected") {
-      this.callback(this.returnV);
-    }
+    this.errcallback.push(callback);
+    console.log("zz", this);
     return this;
   }
 }
@@ -51,15 +42,26 @@ class seokheePromise {
 const promise1 = new seokheePromise(function (resolve, reject) {
   try {
     setTimeout(() => {
-      reject("실패");
       resolve("성공!");
-    }, 1000);
-  } catch {}
+    }, 0);
+  } catch {
+    reject("실패");
+  }
 });
 
 console.log(promise1); // 여기에서 pending이 나오도록
 
 promise1
-  .then((result) => console.log("Promise 성공:", result))
-  .catch((error) => console.log("Promise 실패:", error));
-// console.log(promise1.state, "/ return: ", promise1.returnValue); // Outputs: Success!
+  .then((result) => {
+    console.log(promise1, `\n`, "Promise 성공:", result);
+  })
+  .catch((result) => console.log(promise1, `\n`, "Promise 실패:", result))
+  .then((result) => console.log(promise1, `\n`, "체인", result));
+
+/*
+ * arrow function 없이 this 바인딩 해주기
+ * then, catch 등을 체이닝 => Promise를 반환해야 함
+ *
+ *
+ * ㅁ
+ */
